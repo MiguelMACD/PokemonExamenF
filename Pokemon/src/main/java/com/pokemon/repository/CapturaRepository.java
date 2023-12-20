49b2895f957801452341012cb2932618200fa177
Miguel Cárdenas <101854059+MiguelMACD@users.noreply.github.com>
2023-12-20 09:50:41 -0500

    segundo

--- Pokemon/src/main/java/com/pokemon/repository/CapturaRepository.java
+++ Pokemon/src/main/java/com/pokemon/repository/CapturaRepository.java
@@ -1,5 +1,10 @@
 package com.pokemon.repository;
 
-public interface CapturaRepository {
+import com.pokemon.model.Captura;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public interface CapturaRepository extends JpaRepository<Captura, Integer> {
+    
 }
