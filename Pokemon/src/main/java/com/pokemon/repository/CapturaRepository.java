102c4c1b240f3e77efb488a658b23c67ca560ba2
Miguel Cárdenas <101854059+MiguelMACD@users.noreply.github.com>
2023-12-20 10:06:31 -0500

    f3

--- Pokemon/src/main/java/com/pokemon/repository/CapturaRepository.java
+++ Pokemon/src/main/java/com/pokemon/repository/CapturaRepository.java
@@ -1,6 +1,6 @@
 package com.pokemon.repository;
 
-import com.pokemon.model.Captura;
+import com.pokemon.entities.Captura;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
