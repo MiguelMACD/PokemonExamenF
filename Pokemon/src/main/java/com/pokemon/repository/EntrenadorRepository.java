b8e489baf60161f51bde169aebcb8c9b51099758
Miguel Cárdenas <101854059+MiguelMACD@users.noreply.github.com>
2023-12-20 09:51:04 -0500

    segundo

--- Pokemon/src/main/java/com/pokemon/repository/EntrenadorRepository.java
+++ Pokemon/src/main/java/com/pokemon/repository/EntrenadorRepository.java
@@ -1,5 +1,10 @@
 package com.pokemon.repository;
 
-public interface EntrenadorRepository {
+import com.pokemon.model.Entrenador;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public interface EntrenadorRepository extends JpaRepository<Entrenador, Integer> {
+  
 }
