7e713d725598d1ec367db88d2b373ac5ba82a7e5
Miguel Cárdenas <101854059+MiguelMACD@users.noreply.github.com>
2023-12-20 09:52:07 -0500

    segundo

--- Pokemon/src/main/java/com/pokemon/repository/TipoPokemonRepository.java
+++ Pokemon/src/main/java/com/pokemon/repository/TipoPokemonRepository.java
@@ -1,5 +1,11 @@
 package com.pokemon.repository;
 
-public interface TipoPokemonRepository {
+import com.pokemon.model.TipoPokemon;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public interface TipoPokemonRepository extends JpaRepository<TipoPokemon, Integer> {
+    // Puedes agregar métodos de consulta personalizados si es necesario
 }
+
